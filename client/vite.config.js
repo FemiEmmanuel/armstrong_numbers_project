ff50c0a46a5985d941fb993cd81fc3a4d5855df3
Oluwafemi Olorunyolemi <Phenox08@gmail.com>
2024-11-10 11:34:42 +0100

    Optimize render files

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
-  define: {
-    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+  server: {
+    port: 5173,
   },
 });
